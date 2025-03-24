c07b082ac5ac421d9f83d6a1fa43a405ef19ba8d
mpbusinesscash <mpbusinesscash@gmail.com>
2025-03-24 00:30:12 +0000

    Site Overhaul

--- scripts.js
+++ scripts.js
@@ -1,27 +1,17 @@
-function changePageTitle(newtitle) {
-  document.title = newtitle + " - Souple";
-}
+const interactiveButtons = document.querySelectorAll('#nav-btns img, .box-button');
 
-const interactiveButtons = document.querySelectorAll('.interactiveButtons img');
-
-// Function to swap image on hover
 function swapImageOnHover(imageElement, hoverImagePath) {
   imageElement.addEventListener('mouseover', () => {
     imageElement.src = hoverImagePath;
   });
   imageElement.addEventListener('mouseout', () => {
-    imageElement.src = imageElement.dataset.originalSrc; // Restore original image on mouseout
+    imageElement.src = imageElement.dataset.originalSrc;
   });
 }
 
-// Loop through nav buttons and attach hover event listeners
   interactiveButtons.forEach(button => {
   const originalSrc = button.src;
   const hoverImagePath = originalSrc.replace('.png', '-hovered.png'); 
-
-// Construct hover image path
   button.dataset.originalSrc = originalSrc;
-
-// Store original source for restoration
   swapImageOnHover(button, hoverImagePath);
 });
